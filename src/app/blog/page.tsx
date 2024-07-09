3c173b5c5e4f0befd955d7c51fae9cc87e6f574f
Hikmahx <hikmayousuph@gmail.com>
2024-07-09 15:57:45 +0100

    select component added to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,37 @@
 import { CardPost } from "@/components/CardPost";
 import Paginator from "@/components/Paginator";
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const Blog = () => {
   return (
     <main className="flex min-h-screen flex-col px-4 py-8 container mx-auto">
-      <h1 className="mb-4 font-bold ">Blog</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 container mx-auto gap-x-6 gap-y-8">
+      <h1 className="text-3xl font-bold tracking-tight sm:text-4xl xl:text-5xl prose-h1 my-6">
+        Blog
+      </h1>
+      <div className="w-full flex justify-end my-4">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Sort By" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Sort By</SelectLabel>
+              <SelectItem value="date">Created At</SelectItem>
+              <SelectItem value="title">Title</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-auto gap-x-6 gap-y-8">
         <CardPost />
         <CardPost />
         <CardPost />
