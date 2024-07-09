c17d8324ff1915ffaa2f0e15d0ef55e9d6030685
Hikmahx <hikmayousuph@gmail.com>
2024-07-09 19:35:09 +0100

    data populated from the backend, ui updated

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -11,13 +11,27 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const Blog = () => {
+const getPostsDetails = async (): Promise<any> => {
+  const response = await fetch(`${process.env.NEXT_API_URL}/api/posts`, {
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return await response.json();
+};
+
+const Blog = async () => {
+  const postsDetails = await getPostsDetails();
+  console.log(postsDetails);
+  const posts = postsDetails.posts;
+
   return (
     <main className="flex min-h-screen flex-col px-4 py-8 container mx-auto">
       <h1 className="text-3xl font-bold tracking-tight sm:text-4xl xl:text-5xl prose-h1 my-6">
         Blog
       </h1>
-      <div className="w-full flex justify-end my-4">
+      <div className="w-full flex justify-end mb-12">
         <Select>
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Sort By" />
@@ -32,10 +46,9 @@ const Blog = () => {
         </Select>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-auto gap-x-6 gap-y-8">
-        <CardPost />
-        <CardPost />
-        <CardPost />
-        <CardPost />
+        {posts.map((post: any) => (
+          <CardPost key={post.id} post={post} />
+        ))}
       </div>
       <Paginator />
     </main>
--- src/components/CardPost.tsx
+++ src/components/CardPost.tsx
@@ -9,58 +9,79 @@ import {
 } from "./ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import { Skeleton } from "./ui/skeleton";
 
-export function CardPost() {
+type PostProps = {
+  _id: number;
+  title: string;
+  content: string;
+  hashtags: string[];
+  author: {
+    name: string;
+    avatar: string;
+  };
+  img: string;
+  createdAt: string;
+};
+
+export const CardPost = ({ post }: { post: PostProps }) => {
+  const extractFirstParagraph = (content: string) => {
+    const match = content.match(/<p\s[^>]*>(.*?)<\/p>/);
+    return match ? match[1].trim() : "";
+  };
+
+  const firstParagraph = extractFirstParagraph(post.content);
+
+  const imageLoader = () => {
+    return <Skeleton className="h-[120px] w-[300px] rounded-t-lg" />;
+  };
+
   return (
     <Card className="!prose lg:!prose-xl prose-slate">
       <div className="overflow-hidden">
         <Image
+          // loader={imageLoader}\
+          // placeholder = 'blur'
           className="w-full h-auto !m-0 lg:!m-0 rounded-t-lg"
-          src="https://placehold.jp/500x500.png"
+          src={post.img}
           alt="placeholder"
           width={300}
           height={120}
         />
       </div>
       <CardHeader>
-        <CardTitle>Post Title</CardTitle>
+        <CardTitle>{post.title}</CardTitle>
         <div className="flex gap-2 flex-wrap">
-          <a
-            href="#"
-            className="!no-underline !font-normal !text-sm bg-slate-100 px-4 py-1 rounded-full w-auto"
-          >
-            product
-          </a>
-          <a
-            href="#"
-            className="!no-underline !font-normal !text-sm bg-slate-100 px-4 py-1 rounded-full w-auto"
-          >
-            news
-          </a>
-          <a
-            href="#"
-            className="!no-underline !font-normal !text-sm bg-slate-100 px-4 py-1 rounded-full w-auto"
-          >
-            technology
-          </a>
+          {post.hashtags.map((tag, index) => (
+            <a
+              key={`${tag}-${index}`}
+              href="#"
+              className="!no-underline !font-normal !text-sm bg-slate-100 px-4 py-1 rounded-full w-auto"
+            >
+              {tag}
+            </a>
+          ))}
         </div>
       </CardHeader>
       <CardContent>
-        <p className="">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida
-          velit vel neque semper, at pulvinar enim facilisis...
-        </p>
-        <p className="">
-          <Link href="/blog/123">Read More</Link>
+        <p>{firstParagraph.substring(0, 100)}...</p>
+        <p>
+          <Link href={`/blog/${post._id}`}>Read More</Link>
         </p>
       </CardContent>
       <CardFooter className="flex items-center gap-2">
         <div className="w-10 h-10 bg-slate-300 rounded-full"></div>
         <div className="flex flex-col text-sm">
-          <span className="m-0 ">Jane Doe</span>
-          <span className="m-0">8th July, 2024</span>
+          <span className="m-0 ">{post.author.name}</span>
+          <time dateTime={post.createdAt.slice(0, 10)}>
+            {new Date(post.createdAt).toLocaleString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}
+          </time>
         </div>
       </CardFooter>
     </Card>
   );
-}
+};
