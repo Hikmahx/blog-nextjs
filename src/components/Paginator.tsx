afb69fb0913fde921f6515bb3b35118b6891d427
Hikmahx <hikmayousuph@gmail.com>
2024-07-10 08:28:03 +0100

    react-paginate implemented

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -8,33 +8,44 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "./ui/pagination";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { setCurrentPage } from "@/lib/features/posts/postsSlice";
+import ReactPaginate from "react-paginate";
 
 const Paginator = () => {
+  const { currentPage, postsDetails } = useAppSelector((state) => state.posts);
+  const dispatch = useAppDispatch();
+
+  const pageCount = postsDetails.pageCount || 3;
+
+  const handlePageClick = (event: { selected: number }) => {
+    dispatch(setCurrentPage(event.selected + 1));
+  };
+
   return (
-    <div className="mt-5">
+    <div className="mt-7 flex flex-col items-center justify-between lg:flex-row">
       <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#" isActive>
-              2
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
+        <ReactPaginate
+          breakLabel="..."
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={pageCount}
+          nextLabel={
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          }
+          previousLabel={
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+          }
+          containerClassName="flex items-center space-x-5 mt-4"
+          disabledClassName="text-gray-400"
+          activeClassName="font-semibold text-dull-navy"
+          initialPage={currentPage - 1}
+        />
       </Pagination>
     </div>
   );
