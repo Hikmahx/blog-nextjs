9c4d2bcdc160cd2ca06d249f31b8cdf1643d9b91
Hikmahx <hikmayousuph@gmail.com>
2024-07-15 10:02:11 +0100

    font changed from Inter to Monda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Monda } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "./theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const monda = Monda({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={monda.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
