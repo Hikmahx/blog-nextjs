a7d6dd1d3950a9edb9733760b339e31e1f2b1d7d
Hikmahx <hikmayousuph@gmail.com>
2024-07-16 08:33:41 +0100

    mdx files and styling updated

--- src/components/CardPost.tsx
+++ src/components/CardPost.tsx
@@ -24,57 +24,49 @@ type PostProps = {
 
 function CardPost({ post }: { post: PostProps }) {
   return (
-    <Card className="!prose lg:!prose-xl prose-slate flex flex-col justify-between">
+    <Card className="!prose lg:!prose-xl prose-slate relative grid grid-rows-[auto_1fr_auto] gap-4 border-none shadow-none">
+      <Link href={`/blog/${post.slug}`} className="absolute inset-0"></Link>
       <div className="overflow-hidden">
         <Image
-          className="w-full h-auto !m-0 lg:!m-0 rounded-t-lg"
+          className="w-full h-auto !m-0 lg:!m-0 rounded-none"
           src={post.img}
           alt="placeholder"
           width={300}
           height={120}
         />
       </div>
-      <CardHeader className="py-3 space-y-0">
-        <CardTitle>{post.title}</CardTitle>
-        <div className="flex gap-2 flex-wrap">
-          {post.hashtags.map((tag, index) => (
-            <a
-              key={`${tag}-${index}`}
-              href={`/tags/${tag}`}
-              className="!no-underline !font-normal !text-sm bg-slate-100 px-4 py-1 rounded-full w-auto"
-            >
-              {tag}
-            </a>
-          ))}
+      <CardHeader className="py-1 space-y-0 px-0">
+        <div className="flex text-xs mt-4 gap-1 text-gray-500">
+          <span className="m-0 ">{post.author.name}</span>
+          <span className="">|</span>
+          <span className="text-4xl"></span>
+          <time dateTime={post.createdAt.slice(0, 10)}>
+            {new Date(post.createdAt).toLocaleString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}
+          </time>
         </div>
+        <CardTitle className="!my-4 lg:!my-4">{post.title}</CardTitle>
       </CardHeader>
-      <CardContent className="pb-0">
-        <p className="font-normal text-base">
-          {post.content.slice(0, 90) + "..."}
+      <CardContent className="pb-0 px-0">
+        <p className="font-normal text-base !my-0 lg:!my-0">
+          {post.content.slice(0, 150) + "..."}
         </p>
       </CardContent>
-      <CardFooter className="grid">
-        <Link href={`/blog/${post.slug}`}>Read More</Link>
-
+      <CardFooter className="px-0">
         <div className="flex items-center gap-2 mt-4">
-          <div>
-            <Image
-              className="w-10 h-10 bg-slate-300 !rounded-full !my-2 lg:!my-2"
-              src={post.author.avatar}
-              alt="placeholder"
-              width={40}
-              height={40}
-            />
-          </div>{" "}
-          <div className="flex flex-col text-sm">
-            <span className="m-0 ">{post.author.name}</span>
-            <time dateTime={post.createdAt.slice(0, 10)}>
-              {new Date(post.createdAt).toLocaleString("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "numeric",
-              })}
-            </time>
+          <div className="flex gap-2 flex-wrap relative">
+            {post.hashtags.map((tag, index) => (
+              <a
+                key={`${tag}-${index}`}
+                href={`/tags/${tag}`}
+                className="!no-underline !font-normal !text-sm bg-white border border-black hover:bg-black hover:!text-white transition-all px-4 py-1 rounded-full w-auto"
+              >
+                {tag}
+              </a>
+            ))}
           </div>
         </div>
       </CardFooter>
--- src/layouts/PostsLayout.tsx
+++ src/layouts/PostsLayout.tsx
@@ -13,9 +13,9 @@ type Props = {
 function PostsLayout({ posts, currentPage, pageCount }: Props) {
   return (
     <main className="flex min-h-screen flex-col px-4 py-8 container mx-auto">
-      <h1 className="text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl prose-h1 my-6">
+      {/* <h1 className="text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl prose-h1 my-6">
         Blog
-      </h1>
+      </h1> */}
       <div className="w-full flex justify-end mb-12">
         <SortSelect />
       </div>
