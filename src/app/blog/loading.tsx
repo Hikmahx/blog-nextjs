f7d7a776f0a1e5fa89a4879d6cadc1457fd5d520
Hikmahx <hikmayousuph@gmail.com>
2024-07-22 07:30:57 +0100

    blog loading component updated

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -1,32 +1,37 @@
 import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
+import Search from "@/components/Search";
+import SortSelect from "@/components/SortSelect";
 
 export default function Loading() {
   return (
     <main className="flex min-h-screen flex-col px-4 py-8 container mx-auto">
+      <Search searchTerm="" />
       <div className="w-full flex justify-between mb-4 mt-8">
-        <Skeleton className="h-10 w-3/4 bg-slate-200" />
-        <Skeleton className="h-10 w-1/4 bg-slate-200" />
+      <h1 className="text-2xl font-bold tracking-tight prose-h1">Articles</h1>
+      <SortSelect />
       </div>
       <div className="flex flex-col mx-auto gap-x-6 gap-y-2 w-full">
         {[...Array(5)].map((_, index) => (
           <div
             key={index}
-            className="w-full !prose lg:!prose-xl prose-slate !max-w-none lg:!max-w-none relative rounded-none shadow-none flex items-center justify-between border-white border-b border-b-gray-300 first:border-t first:border-t-gray-300 gap-x-4"
+            className="w-full relative rounded-none shadow-none flex flex-col md:flex-row items-center justify-between border-white border-b border-b-gray-300 first:border-t first:border-t-gray-300 gap-x-4 py-4"
           >
-            <div className="">
+            <div className="order-2 md:order-1 flex-1 w-full md:max-w-lg">
               <div className="py-1 space-y-0 px-0">
                 <div className="flex items-center gap-2">
-                  <Skeleton className="w-10 h-10 bg-slate-200 rounded-full !my-2 lg:!my-2" />
+                  <Skeleton className="w-10 h-10 bg-slate-200 rounded-full my-7" />
                   <div className="flex flex-col text-xs">
+                    <Skeleton className="h-4 w-32 bg-slate-200 mb-2" />
                     <Skeleton className="h-4 w-24 bg-slate-200" />
-                    <Skeleton className="h-4 w-32 bg-slate-200" />
                   </div>
                 </div>
-                <Skeleton className="h-6 w-3/4 bg-slate-200 !my-2 lg:!my-2" />
+                <Skeleton className="h-9 w-3/4 md:w-full bg-slate-200" />
               </div>
-              <div className="pb-0 px-0">
-                <Skeleton className="h-4 w-full bg-slate-200 !my-0 lg:!my-0" />
+              <div className="pb-0 px-0 mb-6 mt-4">
+                <Skeleton className="h-3 w-full bg-slate-200 mb-2" />
+                <Skeleton className="h-3 w-full bg-slate-200 mb-2" />
+                <Skeleton className="h-3 w-3/4 bg-slate-200 mb-2" />
               </div>
               <div className="px-0 mt-4 flex gap-2 flex-wrap">
                 {[...Array(3)].map((_, tagIndex) => (
@@ -37,7 +42,7 @@ export default function Loading() {
                 ))}
               </div>
             </div>
-            <Skeleton className="w-40 h-24 bg-slate-200 rounded-lg" />
+            <Skeleton className="w-full md:w-80 h-40 md:h-60 bg-slate-200 rounded-lg md:order-1" />
           </div>
         ))}
       </div>
